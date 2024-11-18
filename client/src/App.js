10ccbbe993beae91354a8ee738aa450986ba6b1c
Pb78654 <pb78654@gmail.com>
2024-11-18 15:49:34 +0530

    created wallet component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import Navbar from './Components/Navbar'
 import Fotter from './Components/Fotter'
 import SignUp from './Components/SignUp'
 import ParkingSpace from './Components/ParkingSpace';
+import Wallet from './Components/Wallet';
 function App() {
   return (
     
     <div className="App">
      <Navbar/>
      {/* <SignUp/> */}
-     <ParkingSpace/>
+     {/* <ParkingSpace/> */}
+     <Wallet/>
      <Fotter/>
     </div>
     
