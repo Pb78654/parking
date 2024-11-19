71af9ad9bde6290fae9c95b7de38e638f072cf5a
Pb78654 <pb78654@gmail.com>
2024-11-19 13:37:11 +0530

    wallet.css created

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ function App() {
     
     <div className="App">
      <Navbar/>
-     {/* <SignUp/> */}
-     {/* <ParkingSpace/> */}
+     <SignUp/>
+     <ParkingSpace/>
      <Wallet/>
      <Fotter/>
     </div>
